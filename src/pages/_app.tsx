0de3ed0e14becb7ecc2715c7040c7c78b96180bd
xilucks <siun0331@gmail.com>
2021-11-06 00:03:46 +0900

    네비게이션 바 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 // import App from "next/app";
+import 'semantic-ui-css/semantic.min.css'
 import type { AppProps /*, AppContext */ } from 'next/app'
+import Navbar from '../components/Navbar/Navbar';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+
+    </div>
+    
+  );
 }
 
 export default MyApp;
