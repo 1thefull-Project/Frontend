233a1663840206766aebd4b17438a5ff83b1fa13
leeyeonghyeon <yeonghyeon0403@naver.com>
2021-11-12 01:52:39 +0900

    메인화면 카테고리 구현

---
+++ src/components/category.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+import Image from 'next/image';
+
+interface CategoryIcon {
+    img: string;
+    name: string;
+}
+
+export const Category: React.FC<CategoryIcon> = ({img, name}) => {
+    return (
+        <Box>
+                <Image src={img} alt="" width={49} height={48} />
+                <p>{name}</p>
+        </Box>
+    )
+}
+
+const Box = styled.div`
+    width: 70px;
+    height: 61px;
+    margin-bottom: 12px;
+    p{
+        font-family: Asset;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 9px;
+        width: 70px;
+        height: 12px;
+    }
+`;
+
