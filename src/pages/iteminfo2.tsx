8a57b46b6c90f2566c6267c3c15be8172efd01c7
xilucks <siun0331@gmail.com>
2021-11-18 02:52:12 +0900

    상품상세페이지 단락 나누기 및 판매자 정보 컴포넌트 추가

--- src/pages/iteminfo2.tsx
+++ src/pages/iteminfo2.tsx
@@ -8,7 +8,8 @@ interface ItemProps {
     tag: any[];
   }
 
-export default function Iteminfo(item: any){
+
+  export default function Iteminfo(item: any){
     // const {image, title, state, name} = item;
     return(
         <div>
@@ -20,41 +21,55 @@ export default function Iteminfo(item: any){
                         <span>손바닥 보호 실리콘 비닐봉지 손잡이 </span>
                     </TextZone>
                     <Progressbar>
+                        <p>진행바 들어갈 위치</p>
                     </Progressbar>
                     <Price>
-
+                        <p>예상 가격 들어갈 위치</p>
                     </Price>
                 </ItemContent>
                 <Profile>
-
+                    <p>프로필 들어갈 위치</p>
                 </Profile>
 
                 <Menu>
-
+                    <div>
+                        <a>공지사항</a>
+                        <a>제품 정보</a>
+                        <a>댓글</a>
+                        <a>정보 조회</a>
+                    </div>
                 </Menu>
 
                 <ContentZone>
                     <Notice>
                         <MenuTitle>
+                            <div className ='NoticeTitle'>
                             <img src = "/componentImg/HoneyIcon.png"/>
                             <label>공지사항</label>
+                            <img className = "MoreButton" src = "/button/NoticeMoreButton.png" />
+                            </div>
                             <div className = "Noticearea">
                                 <h6>공지 내용 들어갈 공간</h6>
                             </div>
                             <div className = "SecondNoticearea">
-                                <h6>부 공지 내용 들어갈 공간</h6>
                             </div>
                         </MenuTitle>
-
                     </Notice>
+
+                    <img className = "SeperateLine"src = "/detailpage/SeperateLine.png"/>
 
                     <ItemInfomation>
                         <MenuTitle>
                             <img src = "/componentImg/HoneyIcon.png"/>
                             <label>제품 정보</label>
+                            <div className = "InfomationArea"> 
+                                <h6>제품 정보 들어갈 공간</h6>
+                            </div>
                         </MenuTitle>
 
                     </ItemInfomation>
+
+                    <img className = "SeperateLine"src = "/detailpage/SeperateLine.png"/>
 
                     <Comment>
                         <MenuTitle>
@@ -64,12 +79,26 @@ export default function Iteminfo(item: any){
 
                     </Comment>
 
+                    <img className = "SeperateLine"src = "/detailpage/SeperateLine.png"/>
+
                     <SellerInfomation>
                         <MenuTitle>
                             <img src = "/componentImg/HoneyIcon.png"/>
                             <label>정보 조회</label>
-                        </MenuTitle>
+                            <div className = "InfomationBox">
 
+                                    <h6>디테일</h6>
+                            </div>
+
+                            <div className = "InfomationButton">
+                                <div>
+                                    <button className = "SellerView">판매업체 정보 조회</button>
+                                    <button className = "LoveyView">러비 정보 조회</button>
+                                </div>
+                                <button className = "NumberView">운송장 번호 택배 조회</button>
+                            </div>
+
+                        </MenuTitle>
 
                     </SellerInfomation>
 
@@ -77,8 +106,10 @@ export default function Iteminfo(item: any){
                         <img src = "componentImg/SellNotice.png"/>
                     </NoticeImg>
 
-
-                    <IteminfoFooter/>
+                    <div className = "Footer">
+                        <IteminfoFooter/>
+                    </div>
+                    
                 </ContentZone>
             </Write>
          </div>
@@ -86,38 +117,38 @@ export default function Iteminfo(item: any){
     );
 
 }
+
 const Write = styled.div`
-    width: 360px;
-    margin-left:auto;
     margin-right:auto;
+    margin-left:auto;
     justify-content:center;
     display: flex;
     flex-direction: column;
-    text-align: center;
     width:360px;
     text-align: center;
-    
-    
-    
+    .SeperateLine{
+        margin-bottom: 25px;
+    }
+
+    .Footer{
+        width:360px;
+        position:fixed; 
+        bottom:0px; 
+        background: rgba(255,255,255,0.95);
+        
+    }
+
 `
 const ItemContent = styled.div`
-    width:360px;
-    
     img{
-        margin-top:19px;
-        display:flex;
-        width:328px;
-        height: 218px;
-        margin-bottom:13px;
+        width:360px;
     }
+    
+ 
 `
 
 const TextZone = styled.div`
-    width:360px;
-    height: 100%;
-    margin-bottom:32px;
-    label{ 
-       
+       label{ 
         line-height:16px;
         font-family: Roboto;
         font-style: normal;
@@ -136,7 +167,7 @@ const TextZone = styled.div`
         font-family: Roboto;
         font-style: normal;
         font-size: 15px;
-        line-height: 18px;
+        line-height: 18px;c
         display: flex;
         align-items: center;
         font-weight: 400;
@@ -147,8 +178,8 @@ const TextZone = styled.div`
     
 `
 const ContentZone = styled.div`
-    width:360px;
-    
+   text-align:left;
+   
 `
 const Progressbar = styled.div`
 `
@@ -157,38 +188,21 @@ const Price = styled.div`
 const Profile = styled.div`
 `
 const Menu = styled.div`
+    margin-bottom:26px;
+
 `
 const Notice = styled.div`
-    
-    height:100px;
-    .Noticearea{
-        
-        margin-left:auto;
-        margin-right:auto;
-        margin-top:15px;
-        width: 328px;
-        
-        border-radius: 12px;
-        border: 2px solid #FFD15B;
-        margin-bottom:15px;
+    .NoticeTitle{
+        margin-bottom: 14.87px;
     }
-    .SecondNoticearea{
-        margin-left:auto;
-        margin-right:auto;
 
-        width:329px;
-        border-radius: 10px;
-        border: 2px solid gray;   
-    }
+    margin-bottom: 37px;
+    
+    
 `
 const MenuTitle = styled.div`
-
-    height: 21.13px;
-    width: 360px;
-    margin-left: auto;
-    margin-right:auto;
-    text-align:left;
-
+    padding-left:16px;
+    margin-bottom:25px;
     img{
         vertical-align: middle;
         width:21.77px;
@@ -196,21 +210,75 @@ const MenuTitle = styled.div`
     }
     label{
         vertical-align: middle;     
-           
+        
     }
+
+    .Noticearea{
+        text-align:center;
+        width:328px;
+        border-radius: 12px;
+        border: 1px solid #FFD15B;
+    }
+    .MoreButton{
+        margin-left:226.23px;
+        vertical-align: bottom;
+    }
+    
    
 `
 const ItemInfomation = styled.div`
+    
 `
 const Comment = styled.div`
+    text-align:left;
 `
 const SellerInfomation = styled.div`
+    text-align:left;
+
+    .InfomationBox{
+        margin-top:15px;
+        text-align:center;
+        width:328px;
+        border-radius: 12px;
+        border: 1px solid #FFD15B;
+        margin-bottom:15px; 
+    }
+
+    button{
+        width:316px;
+        height:38px;
+        margin-bottom:15px;
+    }
+    
+    .SellerView{
+        width: 150px;
+        margin-left:6px;
+        border-radius: 5px;
+        border: 1px solid #FFD15B;
+        background: #FFFFFF;
+        color: #FFBB0D;
+    }
+    .LoveyView {
+        width: 150px;
+        margin-left:16px;
+        border-radius: 5px;
+        border: 0px;
+        background: #FFD15B;
+        color: #FFFFFF;
+    }
+    .NumberView{
+        width:316px;
+        margin-left:6px;
+        border-radius: 5px;
+        border: 0px;
+        background: #C4C4C4;
+        color: #FFFFFF;
+    }
 `
 const NoticeImg = styled.div`
+    margin-bottom:43px;
+    padding-left:16px;
 
-    img {
-        
-
-        width:360px;
-    }
+    
+  
 `
