9f001d183900d79dd53648b057152a48ad0d593c
xilucks <siun0331@gmail.com>
2021-11-24 23:28:05 +0900

    에러해결

--- src/pages/view/[itemId].tsx
+++ src/pages/view/[itemId].tsx
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from "axios";
-import Iteminfo from '../iteminfo';
+import Iteminfo from '../../components/iteminfo';
 import { GetServerSideProps } from 'next';
 
 
