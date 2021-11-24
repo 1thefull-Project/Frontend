bf88aae38b42a09f749a9484f4d416e9c16667a0
leeyeonghyeon <yeonghyeon0403@naver.com>
2021-11-25 02:47:22 +0900

    getUser 테스트2

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,22 +1,26 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import styled from "@emotion/styled";
 import OrderModal from "../components/Modal/OrderModal";
 import OrderModal2 from "../components/Modal/OrderModal2";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-    const userInfo = await axios.get(process.env.NEXT_PUBLIC_GET_USER as string);
-    return {
-      props: { userInfo: userInfo.data }, 
-    }
-  }
+export default function Order() {
 
-export default function Order({userInfo}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    //로그인한 사용자 정보 불러오기
+    const [userObject, setUserObject] = useState<any>();
+    useEffect(() => {
+        axios.get(process.env.NEXT_PUBLIC_GET_USER as string, { withCredentials: true }).then((res: AxiosResponse) => {
+            if (res.data) {
+                //console.log(res.data);
+                setUserObject(res.data);
+            }
+        })
+    }, [])
 
-    console.log(userInfo);
-    console.log(userInfo.name);
-    console.log(userInfo.address);
+    console.log(userObject);
+    console.log(userObject.name);
+    console.log(userObject.address);
 
     const [isShowing, setIsShowing] = useState(false);
     const [isShowing2, setIsShowing2] = useState(false);
@@ -61,7 +65,7 @@ export default function Order({userInfo}: InferGetServerSidePropsType<typeof get
             <Section>
                 <Box style={{height: '50px'}}>
                     <Horizontal>
-                        <p className='headline'>주문자</p> <p className='blue-text'>{userInfo.name}</p>
+                        <p className='headline'>주문자</p> <p className='blue-text'>{userObject.name}</p>
                     </Horizontal>
                 </Box>
             </Section>
@@ -81,8 +85,8 @@ export default function Order({userInfo}: InferGetServerSidePropsType<typeof get
                 :               //기본 결제수단 불러오기
                     <Box style={{height: '119px'}}>
                         <p className='headline'>배송지</p>
-                        <p className='basic-text'>{userInfo.address}</p>
-                        <p className='basic-text'>{userInfo.phone}</p>
+                        <p className='basic-text'>{userObject.address}</p>
+                        <p className='basic-text'>{userObject.phone}</p>
                     </Box>
                 }
                 
